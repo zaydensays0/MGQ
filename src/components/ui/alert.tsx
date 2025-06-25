a07bfa4095aa0201ab31d84cf0da2b707010c4d0
Mzusu <mzusu09@gmail.com>
2025-06-25 01:12:14 +0000

    Make the following changes:
    ,
    
    Find this element:
    
      <Alert variant="de

--- src/components/ui/alert.tsx
+++ src/components/ui/alert.tsx
@@ -10,7 +10,7 @@ const alertVariants = cva(
       variant: {
         default: "bg-background text-foreground",
         destructive:
-          "border-destructive/50 text-destructive dark:border-destructive [&>svg]:text-destructive",
+          "border-transparent bg-destructive text-destructive-foreground [&>svg]:text-destructive-foreground",
       },
     },
     defaultVariants: {
